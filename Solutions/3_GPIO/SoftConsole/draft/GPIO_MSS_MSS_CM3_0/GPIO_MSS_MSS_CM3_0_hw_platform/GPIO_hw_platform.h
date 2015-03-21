6845569a0b915675f42bf3c6778d078551be4877
Jeroen Doggen <jeroendoggen@gmail.com>
2015-03-21 16:13:28 +0100

    opgave 3: GPIO (draft- just turning on/off leds)

---
+++ Solutions/3_GPIO/SoftConsole/draft/GPIO_MSS_MSS_CM3_0/GPIO_MSS_MSS_CM3_0_hw_platform/GPIO_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef GPIO_HW_PLATFORM_H_
+#define GPIO_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Sat Mar 21 15:51:57 2015
+*
+*Memory map specification for peripherals in GPIO
+*/
+
+/*-----------------------------------------------------------------------------
+* MSS_CM3_0 subsystem memory map
+* Master(s) for this subsystem: MSS_CM3_0 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* GPIO_HW_PLATFORM_H_*/
